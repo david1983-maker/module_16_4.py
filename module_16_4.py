e3c3bac57101012c68b73195db1faf62a929f2c7
david1983-maker <davidkilidzan94127@gmail.com>
2024-11-17 13:12:47 +0300

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Path, Body, HTTPException
 from pydantic import BaseModel
-from typing import Annotated
+from typing import List
 
 app = FastAPI()
 # users = {'1': "Имя:Example,возраст:18"}
@@ -19,35 +19,34 @@ async def get_user() -> list[User]:
 
 
 @app.post('/user/{username}/{age}')
-async def post_user(user: User, username: str, age: int) -> User:
+async def post_user(username: str, age: int) -> User:
     if users:
-        user.id = users[-1].id
+        user_id = users[-1].id +1
     else:
-        user.id = 1
-    new_user = User(id=user.id, username=username, age=age)
+        user_id = 1
+    new_user = User(id=user_id, username=username, age=age)
     users.append(new_user)
     return new_user
 
 
 @app.put('/user/{user_id}/{username}/{age}')
 async def update_user(user_id: str, username: str, age: int, user: str = Body()) -> User:
-    try:
+   
         for user in users:
-            if user_id in users:
+            if user.id == users_id:
                 user.username = username
                 user.age = age
                 return user
-    except StopIteration:
-        raise HTTPException(status_code=404, detail="User was not found")
-
+            else:
+                raise HTTPException(status_code=404, detail="User was not found")
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: str) -> User:
+async def delete_user(user_id: int) -> str:
     try:
-        for user in users:
-            if user_id in users:
-                users.remove(user)
+        users.pop(user_id)
+        return f'{user_id} DELETE'
 
-                return user
     except StopIteration:
         raise HTTPException(status_code=404, detail="User was not found")
+
+
